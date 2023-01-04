587453b7d6b3cbc82d4b39de2fb7cfbd5c208f25
ShivaGaneshN <nerella.shivaganesh04@gmail.com>
2023-01-04 19:35:24 +0530

    crud operations done

---
+++ posts/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from posts import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('create', views.create, name='create'),
+    path('retrieve', views.retrieve, name='retrieve'),
+    path('edit/<int:id>', views.edit, name='edit'),
+    path('update/<int:id>', views.update, name='update'),
+    path(r'^delete_product/(?P<pk>[0-9]+)/$', views.delete, name='delete'),
+]
